ae3d08a6fbea935e63522a0779035ec2b842daa8
sneelyg <santiagoneely@gmail.com>
2022-09-10 01:23:13 +0000

    oretty good

--- src/models.py
+++ src/models.py
@@ -8,25 +8,46 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+class User(Base):
+    __tablename__ = 'user'
+    user_ID = Column(Integer, primary_key=True)
+    username = Column(String(20))
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=True)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Characters(Base):
+    __tablename__ = 'characters'
+    char_ID = Column(Integer, primary_key=True)
+    char_name = Column(String(50), nullable=False )
+    dimension = Column(String(250), nullable=True)
+    origin = Column(String(250), nullable=False)
+    status = Column(String(10), nullable=False)
 
-    def to_dict(self):
+
+class Episodes(Base):
+    __tablename__ = 'episodes'
+    Ep_ID = Column(Integer, primary_key=True)
+    title = Column(String(50), nullable=False )
+    Air_Date = Column(String(250), nullable=False)
+    
+
+
+class Ep_Favorites(Base):
+    __tablename__ = 'ep_favorites'
+    fav_ep_id = Column(Integer, primary_key=True)
+    user_id = Column(String(250), ForeignKey ("user.user_ID"))
+    ep_ID = Column(Integer, ForeignKey ("episodes.ep_ID"))
+    rel = relationship(Episodes)
+    rel= relationship (Characters)
+
+class Char_Favorites(Base):
+    __tablename__ = 'char_favorites'
+    fav_char_id = Column(Integer, primary_key=True)
+    user_id = Column(String(250), ForeignKey("user.user_ID"))
+    char_ID = Column(Integer, ForeignKey("characters.char_ID"))
+
+def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
